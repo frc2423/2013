a3a15bb4062f00172521a1f922b86fe27eaf4e41
Ethan Cox <ethanecx@gmail.com>
2013-02-01 20:42:00 -0500

    Finshed electrical_test(for now)

--- robot/electrical_test/robot.py
+++ robot/electrical_test/robot.py
@@ -19,12 +19,10 @@ r_motor = wpilib.Jaguar(1)
 #Variables
 switch_1 = "Switch 1"
 switch_2 = "Switch 2"
-shootermotor = "Shooter Motor"
-anglemotor = "Angle Motor"
-feedmotor = "Feed Motor"
-lmotor = "Left Drive Motor"
-rmotor = "Right Drive Motor"
-shooterencoder = "Shooter Encoder"
+AngleMotor = "Angle Motor"
+FeedMotor = "Feed Motor"
+ShooterEncoder = "Shooter Encoder"
+FrisbeeCount = "Frisbee Count"
 #CAN Motoros
 shooter_motor = wpilib.CANJaguar(1)
 angle_motor = wpilib.CANJaguar(2)
@@ -34,7 +32,7 @@ shooter_encoder = wpilib.AnalogChannel(1)
 #needs to be connected to jaguar
 #chamber_sensor dectets frisbees being held
 chamber_sensor = wpilib.AnalogChannel(5)
-
+#switches limit angle motor movement
 switch1 = wpilib.DigitalInput(1)
 switch2 = wpilib.DigitalInput(2)
 
@@ -81,25 +79,33 @@ class MyRobot(wpilib.SimpleRobot):
         timer = wpilib.Timer()
         timer.Start()
 
-        self.drive.ArcadeDrive(stick1)
+        
         
         while self.IsOperatorControl() and self.IsEnabled():
+            self.drive.ArcadeDrive(stick1)
             #Feed Motor
             if stick2.GetTrigger():
                 feed_motor.Set(feed_motor_spd)
-            if stick2.GetRawButton(4)
-                feed_motor.GetPosition()
+            if stick2.GetRawButton(1):
+                feed_motor_postion = feed_motor.GetPosition()
             #Shooter Motor
             if stick2.GetRawButton(2):
                 shooter_motor_spd = 1
                 shooter_motor.Set(shooter_motor_spd)
             #display on SmartDashboard current spd and set spd
             #Angle Motor
-            if stick1.GetRawButton(3):
-                angle_motor.GetPosition()
+            if stick2.GetRawButton(3):
+                angle_motor_position = angle_motor.GetPosition()
+            if stick2.GetRawButton(4):
+                frisbee_count = chamber_sensor.Get()
+            if stick2.GetRawButton(5):
+                shooter_encoder_position = shooter_encoder.Get()
             dog.Feed()
             wpilib.Wait(0.04)
-
+            SmartDashboard.PutDouble(FeedMotor, feed_motor_spd)
+            SmartDashboard.PutDouble(AngleMotor, angle_motor_position)
+            SmartDashboard.PutDouble(FrisbeeCount, frisbee_count)
+            SmartDashboard.PutDouble(ShooterEncoder, shooter_encoder_position)
 def run():
     robot = MyRobot()
     robot.StartCompetition()
