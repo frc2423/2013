785c167347e1b6686be33239e2936ec504df4574
arsh97 <punjabiking19@gmail.com>
2013-02-02 15:15:26 -0500

    defining shooting and currently trying to compare desired angle and speed to actual ones.

--- robot/source/components/shooter.py
+++ robot/source/components/shooter.py
@@ -3,9 +3,31 @@ try:
 except ImportError:
     import fake_wpilib as wpilib
     
-shooter_motor = wpilib.CANJaguar(3)
 
 class shooter(object):
     
-    def __init__(self,shooter_motor,switch2,switch1,):
-        '''Parameters are shooter_motor, switch2, and switch 1... MTBA'''
+    def __init__(self, motorA, motorS, sensorA, sensorS, shooter_jag, d_speed, d_angle):
+        self.updated = False
+        self.shooter_jag = shooter_jag
+        self.sensorA = sensorA
+        self.sensorS = sensorS
+             
+    def shoot(self, speed, angle):
+        self.shooter_jag
+        self.sensorA
+        self.sensorS 
+        self.speed = speed
+        self.angle = angle
+        if d_speed = speed and d_angle = angle:
+            self.updated = True
+        else:
+             self.updated = False
+        
+    
+    def update(self):
+        if self.updated == True:
+            shooter_jag.shoot(self.angle, self.speed)
+        else:
+            self.updated = False
+            
+            
