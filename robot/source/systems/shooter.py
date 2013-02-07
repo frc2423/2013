3da8cea8ac770e8e1829aeba1a3fb5344ca088cd
Ethan Cox <ethanecx@gmail.com>
2013-02-06 19:45:18 -0500

    Updated Shooter System

---
+++ robot/source/systems/shooter.py
@@ -0,0 +1,35 @@
+from components import init
+
+class Shooter():
+
+    def __init__():
+        
+        self.Updated = None
+        
+    def ShootWhenReady():
+        
+        if GetFrisbeeCount > 0
+            
+            if shooter_wheel.IsReadyAngle() == True and shooter_wheel.IsReadySpeed() == True:
+            
+                self.Updated = True
+                
+    def ContinousShoot
+    
+        while GetFrisbeeCount > 0
+        
+            if shooter_wheel.IsReadyAngle() == True and shooter_wheel.IsReadySpeed() == True:
+            
+                self.Updated = True
+            
+    def ShootNow():
+    
+        self.Updated = True
+            		
+    def Update():
+        
+        if self.Updated == True
+        
+            feeder.Feed()
+            self.Updated = None
+     
