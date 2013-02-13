ba06233fec55f93c69a4ae464288e22569277316
arsh97 <punjabiking19@gmail.com>
2013-02-12 20:53:31 -0500

    finished for now, ports are right

--- robot/electrical_test/robot.py
+++ robot/electrical_test/robot.py
@@ -1,9 +1,11 @@
 print( "Loading: Kwarqs2013 electrical testing program" )
 
+from components import generic_distance_sensor
 try:    
     import wpilib
 except ImportError:
     import fake_wpilib as wpilib
+    
     
 #Variables
 switch_1 = "Switch 1"
@@ -21,14 +23,17 @@ stick2 = wpilib.Joystick(2)
 l_motor = wpilib.Jaguar(2)
 r_motor = wpilib.Jaguar(1)
 
-#CAN Motoros
-shooter_motor = wpilib.CANJaguar(1)
-angle_motor = wpilib.CANJaguar(2)
-feed_motor = wpilib.CANJaguar(3)
+#CAN Motors
+shooter_motor = wpilib.CANJaguar(7)
+angle_motor = wpilib.CANJaguar(4)
+feed_motor = wpilib.CANJaguar(5)
 
 #needs to be connected to jaguar
-#chamber_sensor dectets frisbees being held
-chamber_sensor = wpilib.AnalogChannel(5)
+#chamber_sensor detects frisbees being held
+chamber_sensor = generic_distance_sensor.GenericDistanceSensor(3, generic_distance_sensor.E4P_250_250)
+feed_sensor = generic_distance_sensor.GenericDistanceSensor(4, generic_distance_sensor.E4P_250_250)
+shooter_sensor = generic_distance_sensor.GenericDistanceSensor(5, generic_distance_sensor.E4P_250_250)
+
 #Motor Speeds
 feed_motor_spd = 1
 
@@ -78,23 +83,23 @@ class MyRobot(wpilib.SimpleRobot):
             #Shooter Motor
             
             shooter_motor_spd = 1
-            shooter_motor.Set(GetZ)
+            shooter_motor.Set(stick2.GetZ())
             
             #display on SmartDashboard current spd and set spd
             #Angle Motor
             
             
             angle_motor_position = angle_motor.GetPosition()
-            angle_motor.Set(GetY)
+            angle_motor.Set(stick2.GetY())
                 
             frisbee_count = chamber_sensor.Get()
                 
             shooter_encoder_position = shooter_motor.GetPosition()
             
-            wpilib.SmartDashboard.PutDouble(feedMotor, feed_motor_spd)
-            wpilib.SmartDashboard.PutDouble(angleMotor, angle_motor_position)
-            wpilib.SmartDashboard.PutDouble(frisbeeCount, frisbee_count)
-            wpilib.SmartDashboard.PutDouble(shooterEncoder, shooter_encoder_position)
+            self.wpilib.SmartDashboard.PutNumber(feedMotor, feed_motor_spd)
+            self.wpilib.SmartDashboard.PutNumber(angleMotor, angle_motor_position)
+            self.wpilib.SmartDashboard.PutNumber(frisbeeCount, frisbee_count)
+            self.wpilib.SmartDashboard.PutNumber(shooterEncoder, shooter_encoder_position)
             
             dog.Feed()
             wpilib.Wait(0.04)
