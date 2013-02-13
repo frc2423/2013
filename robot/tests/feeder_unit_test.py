7144eb97b2d78f9389ad9a9a703f216681db302c
Dustin Spicuzza <dustin@virtualroadside.com>
2013-02-13 11:31:59 -0500

    Updates to the unit test for the feeder to be more realistic

--- robot/tests/feeder_unit_test.py
+++ robot/tests/feeder_unit_test.py
@@ -11,6 +11,7 @@ import_robot = False
 
 import inspect
 import fake_wpilib as wpilib
+import random
 
 def lineno():
     #returns current line
@@ -29,68 +30,70 @@ class Test(object):
     def test_get_frisbee_count(self):
         from components import feeder
         self.frisbee_sensor.voltage = 0
-        if hasattr(self.tested_feeder, "get_frisbee_count"):
-            if self.tested_feeder.get_frisbee_count() != 0:
-                raise Exception("Wrong Frisbee Count")
-            
-            self.frisbee_sensor.voltage = feeder.ZERO_FRISBEE
-            
-            if self.tested_feeder.get_frisbee_count() != 0:
-                raise Exception("Wrong Frisbee Count")
-            
-            self.frisbee_sensor.voltage = feeder.ONE_FRISBEE
-            
-            if self.tested_feeder.get_frisbee_count() != 1:
-                raise Exception("Wrong Frisbee Count")
-            
-            self.frisbee_sensor.voltage = feeder.TWO_FRISBEE
-            
-            if self.tested_feeder.get_frisbee_count() != 2:
-                raise Exception("Wrong Frisbee Count")
-            
-            self.frisbee_sensor.voltage = feeder.THREE_FRISBEE
-            
-            if self.tested_feeder.get_frisbee_count() != 3:
-                raise Exception("Wrong Frisbee Count")
-            
-            self.frisbee_sensor.voltage = feeder.FOUR_FRISBEE
-            
-            if self.tested_feeder.get_frisbee_count() != 4:
-                raise Exception("Wrong Frisbee Count")
-        else:
+        if not hasattr(self.tested_feeder, "get_frisbee_count"):
             raise Exception("Please use proper naming conventions")
+            
+        if self.tested_feeder.get_frisbee_count() != 0:
+            raise Exception("Wrong Frisbee Count")
+        
+        self.frisbee_sensor.voltage = random.uniform(feeder.ZERO_FRISBEE, feeder.ONE_FRISBEE)
+        
+        if self.tested_feeder.get_frisbee_count() != 0:
+            raise Exception("Wrong Frisbee Count (expected 0; voltage: %s)" % self.frisbee_sensor.voltage)
+        
+        self.frisbee_sensor.voltage = random.uniform(feeder.ONE_FRISBEE, feeder.TWO_FRISBEE)
+        
+        if self.tested_feeder.get_frisbee_count() != 1:
+            raise Exception("Wrong Frisbee Count (expected 1; voltage: %s)" % self.frisbee_sensor.voltage)
+        
+        self.frisbee_sensor.voltage = random.uniform(feeder.TWO_FRISBEE, feeder.THREE_FRISBEE)
+        
+        if self.tested_feeder.get_frisbee_count() != 2:
+            raise Exception("Wrong Frisbee Count (expected 2; voltage: %s)" % self.frisbee_sensor.voltage)
+        
+        self.frisbee_sensor.voltage = random.uniform(feeder.THREE_FRISBEE, feeder.FOUR_FRISBEE)
+        
+        if self.tested_feeder.get_frisbee_count() != 3:
+            raise Exception("Wrong Frisbee Count (expected 3; voltage: %s)" % self.frisbee_sensor.voltage)
+        
+        self.frisbee_sensor.voltage = random.uniform(feeder.FOUR_FRISBEE, 5)
+        
+        if self.tested_feeder.get_frisbee_count() != 4:
+            raise Exception("Wrong Frisbee Count (expected 4; voltage: %s)" % self.frisbee_sensor.voltage)
+            
         
     def test_feed(self):
         from components import feeder
-        if getattr(self.tested_feeder, "FEEDER_READY_DISTANCE") :
-            self.frisbee_sensor.distance = 1000
-            self.frisbee_sensor.voltage = 3
-            if getattr(self.tested_feeder, "feed"):
-                self.feed()
-                
-                if self.feeder_motor.speed != 0:
-                    raise Exception("Update must be called to start the motor")
-                self.update()
-                
-                if self.feeder_motor.speed != 0:
-                    raise Exception("Feeder is not ready, should not start")
-                            
-                self.frisbee_sensor.distance = 10
-                self.frisbee_sensor.voltage = 1
-                self.feed()
-                if self.feeder_motor.speed != 0:
-                    raise Exception("Update must be called to start the motor")
-                self.update()
-                if self.feeder_motor.speed != 1:
-                    raise Exception("Feeder is ready, should go at full speed")
-                self.update()
-                if self.feeder_motor.speed != 0:
-                    raise Exception("Feeder made full circle should stop")
-                
-            else:
-                raise Exception("Need to set a distance at which the feeder will be ready")
-        else:
+        if not getattr(self.tested_feeder, "FEEDER_READY_DISTANCE") :
             raise Exception("Need to set a distance at which the feeder will be ready")
+        
+        self.frisbee_sensor.distance = 1000
+        self.frisbee_sensor.voltage = 3
+        if not getattr(self.tested_feeder, "feed"):
+            raise Exception("Need to set a distance at which the feeder will be ready")
+            
+        self.tested_feeder.feed()
+        
+        if self.feeder_motor.speed != 0:
+            raise Exception("Update must be called to start the motor")
+        self.update()
+        
+        if self.feeder_motor.speed != 0:
+            raise Exception("Feeder is not ready, should not start")
+                    
+        self.frisbee_sensor.distance = 10
+        self.frisbee_sensor.voltage = 1
+        self.feed()
+        if self.feeder_motor.speed != 0:
+            raise Exception("Update must be called to start the motor")
+        self.update()
+        if self.feeder_motor.speed != 1:
+            raise Exception("Feeder is ready, should go at full speed")
+        self.update()
+        if self.feeder_motor.speed != 0:
+            raise Exception("Feeder made full circle should stop")
+            
+        
    
         
 class Test_Sensor(wpilib.AnalogChannel):
