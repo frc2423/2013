f67d8279d5c4fe7f37a64d92ecafa7bcdb59ca97
arsh97 <punjabiking19@gmail.com>
2013-02-12 20:17:12 -0500

    #comments done. Finished shooter_wheel for now.

--- robot/source/components/shooter_wheel.py
+++ robot/source/components/shooter_wheel.py
@@ -23,17 +23,18 @@ class ShooterWheel(object):
     def set_angle(self, d_angle):
         self.updated = True
         self.d_angle = d_angle
-        #sets the angle if it is not ready
+        #stores the past parameter w/ the instance
     
     def set_speed(self, d_speed):
         self.updated = True
         self.d_speed = d_speed
-        #Sets the speed if it is not ready
+        #Stores the past parameter w/ the instance
         
     def stop(self):
         self.updated = True
         self.d_angle = 0
         self.d_speed = 0
+        #sets the two motors to stop when called
         
     def is_ready_angle(self, d_angle): 
         if self.current_angle() >= (self.d_angle - self.angle_threshold) and self.current_angle() <= (self.d_angle + self.angle_threshold):
@@ -41,15 +42,16 @@ class ShooterWheel(object):
             
         else:
             return False
+        #returns true if current angle is within threshold.
         
    
     def is_ready_speed(self, d_speed):
-        #returns true if desired speed and current speed are equal.
         if self.current_speed() >= (self.d_speed - self.speed_threshold) and self.current_speed() <= (self.d_speed + self.speed_threshold):
             return True
-            
+
         else:
             return False
+        #returns true if current speed is within threshold.
         
     def is_ready(self):
         print ('self.d_angle =', self.d_angle)
@@ -59,48 +61,51 @@ class ShooterWheel(object):
         if self.is_ready_angle(self.d_angle) and self.is_ready_speed(self.d_speed) == True:
             return True
         else:
-            return False 
+            return False
+        #returns true if the ShooterWheel is ready to shoot 
 
 
     def update(self):
         self.updated = True
+        
+        self.pre_angle = self.current_angle()
+        self.pre_speed = self.current_speed()
+        self.pre_d_angle = self.d_angle
+        self.pre_d_speed = self.d_speed
+        self.pre_is_ready_angle = self.is_ready_angle(self.d_angle)
+        self.pre_is_ready_speed = self.is_ready_speed(self.d_speed)
+        
         if self.pre_angle != self.current_angle():
             wpilib.SmartDashboard.PutNumber('current_angle', self.current_angle())
         if self.pre_d_angle != self.d_angle:
-            wpilib.SmartDashboard.PutNumber('d_angle', self.d_angle)
+            wpilib.SmartDashboard.PutNumber('desired_angle', self.d_angle)
         if self.pre_is_ready_angle != self.is_ready_angle(self.d_angle):
             wpilib.SmartDashboard.PutBoolean('is_ready_angle', self.is_ready_angle(self.d_angle))
-
 
         if self.pre_speed != self.current_speed():
             wpilib.SmartDashboard.PutNumber('current_speed', self.current_speed())
         if self.pre_d_speed != self.d_speed:
-            wpilib.SmartDashboard.PutNumber('d_speed', self.d_speed)
+            wpilib.SmartDashboard.PutNumber('desired_speed', self.d_speed)
         if self.pre_is_ready_speed != self.is_ready_speed(self.d_speed):
-            wpilib.SmartDashboard.PutBoolean('is_ready_speed', self.is_ready_speed(self.d_speed)        
+            wpilib.SmartDashboard.PutBoolean('is_ready_speed', self.is_ready_speed(self.d_speed))
+        #prints to SmartDashboard when the (6) values change        
         
         
-         if self.is_ready_angle == True():
+        if self.is_ready_angle(self.d_angle) == True:
             self.angle_jag.set(0)
         
         else:
             self.angle_jag.set(1)
        
         
-        if self.is_ready_speed() == True:
+        if self.is_ready_speed(self.d_speed) == True:
             self.shooter_jag.set(0)
         
         else:
             self.shooter_jag.set(1)
-            
+        #coast if the angle/speed is ready, full power if not.    
             
             self.updated = False    
-            self.pre_angle = self.current_angle()
-            self.pre_speed = self.current_speed()
-            self.pre_d_angle = self.d_angle
-            self.pre_d_speed = self.d_speed
-            self.pre_is_ready_angle = self.is_ready_angle(self.d_angle)
-            self.pre_is_ready_speed = self.is_ready_speed(self.d_speed)
             
         
             
