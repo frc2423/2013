428f55be46b7d7c5072681bcd3e67d953f3727d1
Dustin Spicuzza <dustin@virtualroadside.com>
2013-02-14 15:32:13 -0500

    Fix the electrical test program so that it works, and passes unit tests

--- robot/electrical_test/robot.py
+++ robot/electrical_test/robot.py
@@ -1,19 +1,10 @@
+
 print( "Loading: Kwarqs2013 electrical testing program" )
 
-#from components import generic_distance_sensor
 try:    
     import wpilib
 except ImportError:
     import fake_wpilib as wpilib
-    
-    
-#Variables
-switch_1 = "Switch 1"
-switch_2 = "Switch 2"
-angleMotor = "Angle Motor"
-feedMotor = "Feed Motor"
-shooterEncoder = "Shooter Encoder"
-frisbeeCount = "Frisbee Count"
 
 #Joysticks
 stick1 = wpilib.Joystick(1)
@@ -25,22 +16,29 @@ r_motor = wpilib.Jaguar(1)
 
 #CAN Motors
 shooter_motor = wpilib.CANJaguar(7)
+shooter_motor.SetSpeedReference(wpilib.CANJaguar.kSpeedRef_QuadEncoder)
+shooter_motor.ConfigEncoderCodesPerRev(360)
+shooter_motor.ConfigNeutralMode(wpilib.CANJaguar.kNeutralMode_Brake)
+
 angle_motor = wpilib.CANJaguar(4)
-feed_motor = wpilib.CANJaguar(6)
+angle_motor.SetPositionReference(wpilib.CANJaguar.kPosRef_Potentiometer)
+angle_motor.ConfigPotentiometerTurns(1)
+angle_motor.ConfigNeutralMode(wpilib.CANJaguar.kNeutralMode_Brake)
 
+loader_cam_motor = wpilib.CANJaguar(6)
+
+
+# optical sensors
+loader_sensor = wpilib.AnalogChannel(3)
+loader_cam_sensor = wpilib.AnalogChannel(4)
+shooter_detect_sensor = wpilib.AnalogChannel(5)
+
+
+# drive object
 drive = wpilib.RobotDrive(l_motor, r_motor)
+
+# TODO: Fix this. 
 drive.SetSafetyEnabled(False)
-
-#needs to be connected to jaguar
-#chamber_sensor detects frisbees being held
-#chamber_sensor = generic_distance_sensor.GenericDistanceSensor(3, generic_distance_sensor.E4P_250_250)
-#feed_sensor = generic_distance_sensor.GenericDistanceSensor(4, generic_distance_sensor.E4P_250_250)
-#shooter_sensor = generic_distance_sensor.GenericDistanceSensor(5, generic_distance_sensor.E4P_250_250)
-
-#Motor Speeds
-feed_motor_spd = 1
-
-feed_motor_postion = feed_motor.GetPosition()
 
 class MyRobot(wpilib.SimpleRobot):
 
@@ -80,32 +78,31 @@ class MyRobot(wpilib.SimpleRobot):
         while self.IsOperatorControl() and self.IsEnabled():
             drive.ArcadeDrive(stick1)
             
-            #Feed Motor
+            # loader cam motor
             if stick1.GetTrigger():
-                feed_motor.Set(stick1.GetZ())
+                loader_cam_motor.Set(stick1.GetZ())
                 
-            #Shooter Motor
+            # shooter wheel motor
             if stick2.GetTrigger():
                 shooter_motor.Set(stick2.GetZ())
             
-            #display on SmartDashboard current spd and set spd
-            #Angle Motor
-            
-            
-            angle_motor_position = angle_motor.GetPosition()
+            # Angle motor
             angle_motor.Set(stick2.GetY())
                 
-            #frisbee_count = chamber_sensor.Get()
-                
-            shooter_encoder_position = shooter_motor.GetPosition()
+            # sensor status
+            wpilib.SmartDashboard.PutNumber('Loader', loader_sensor.GetVoltage())
+            wpilib.SmartDashboard.PutNumber('Loader cam', loader_cam_sensor.GetVoltage())
+            wpilib.SmartDashboard.PutNumber('Shooter detect', shooter_detect_sensor.GetVoltage())
             
-            #self.wpilib.SmartDashboard.PutNumber(feedMotor, feed_motor_spd)
-            wpilib.SmartDashboard.PutNumber(angleMotor, angle_motor_position)
-            #self.wpilib.SmartDashboard.PutNumber(frisbeeCount, frisbee_count)
-            wpilib.SmartDashboard.PutNumber(shooterEncoder, shooter_encoder_position)
+            # motor status
+            wpilib.SmartDashboard.PutNumber('Angle', angle_motor.GetPosition())
+            wpilib.SmartDashboard.PutNumber('Shooter wheel speed', shooter_motor.GetSpeed())
+        
             
             dog.Feed()
             wpilib.Wait(0.04)
+            
+        dog.SetEnabled(False)
             
             
 def run():
