5f486f7db9cc51046cc582e04181c71c70af9946
Ethan Cox <ethanecx@gmail.com>
2013-02-16 13:58:44 -0500

    Updated feeder and shooter

--- robot/source/systems/shooter.py
+++ robot/source/systems/shooter.py
@@ -3,17 +3,24 @@ from components import feeder
 
 class Shooter():
 
-    def __init__(self,):
+    def __init__(self, d_angle, d_speed):
         
         self.Updated = None
-        self.d_angle = shooter_wheel.ShooterWheel.d_angle
-        self.d_speed = shooter_wheel.ShooterWheel.d_speed
+        self.d_angle = d_angle
+        self.d_speed = d_speed
     def shoot_when_ready(self):
             
             if shooter_wheel.ShooterWheel.is_ready_angle(self, self.d_angle) == True and shooter_wheel.ShooterWheel.is_ready_speed() == True:
             
                 self.Updated = True
                 
+            elif shooter_wheel.ShooterWheel.is_ready_angle(self, self.d_angle) == False and shooter_wheel.ShooterWheel.is_ready_speed() == True:
+                
+                self.updated = 3
+                
+            elif shooter_wheel.ShooterWheel.is_ready_angle(self, self.d_angle) == True and shooter_wheel.ShooterWheel.is_ready_speed() == False:
+                
+                self.updated = 4
     def continous_shoot(self):
     
         while feeder.Feeder.get_frisbee_count() > 0:
@@ -21,10 +28,26 @@ class Shooter():
             if shooter_wheel.ShooterWheel.is_ready_angle() == True and shooter_wheel.ShooterWheel.is_ready_speed() == True:
             
                 self.Updated = True
+                
+            elif shooter_wheel.ShooterWheel.is_ready_angle(self, self.d_angle) == False and shooter_wheel.ShooterWheel.is_ready_speed() == True:
+                
+                self.updated = 3
+                
+            elif shooter_wheel.ShooterWheel.is_ready_angle(self, self.d_angle) == True and shooter_wheel.ShooterWheel.is_ready_speed() == False:
+                
+                self.updated = 4
             
     def shoot_now(self):
     
-        self.Updated = True
+        self.updated = True
+        
+    def set_angle(self):
+        
+        self.updated = 1 
+        
+    def set_speed(self):
+        
+        self.updated = 2
         
     def update(self):
         
@@ -32,3 +55,19 @@ class Shooter():
         
             feeder.Feeder.feed()
             self.Updated = None
+            
+        if self.updated == 1:
+            
+            shooter_wheel.ShooterWheel.set_angle(self, self.d_angle)
+            
+        if self.updated == 2:
+            
+            shooter_wheel.ShooterWheel.set_angle(self, self.d_speed)
+            
+        if self.updated == 3:
+            
+            self.set_angle()
+        
+        if self.updated == 4:
+            
+            self.set_speed()
