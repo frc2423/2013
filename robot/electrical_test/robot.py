692588c433464a4fdf8b413ad7c3d8cd8978590e
Dustin Spicuzza <dustin@virtualroadside.com>
2013-02-16 02:27:42 -0500

    Update electrical test to add valves, separate ports from object creation

--- robot/electrical_test/robot.py
+++ robot/electrical_test/robot.py
@@ -6,34 +6,78 @@ try:
 except ImportError:
     import fake_wpilib as wpilib
 
+#
+#    Declare all the ports and channels here
+#    Note: these are shared between the electrical test and main code!
+#
+
+# PWM channels
+l_motor_pwm = 1
+r_motor_pwm = 2
+
+# CAN channels
+loader_cam_can = 3
+angle_motor_can = 4
+shooter_wheel_can = 7
+
+# Relay channels
+camera_led_relay = 1
+compressor_relay = 2
+
+# Analog channels
+loader_sensor_analog = 3
+loader_cam_sensor_analog = 4
+shooter_detect_sensor = 5
+
+# Digital channels
+compressor_switch = 1
+
+# Solenoids
+valve1_channel = 1
+valve2_channel = 2
+
+
+#
+#    Create robot motors/sensors here
+#
+
+
 #Joysticks
 stick1 = wpilib.Joystick(1)
 stick2 = wpilib.Joystick(2)
 
 #PWM Motors
-l_motor = wpilib.Jaguar(1)
-r_motor = wpilib.Jaguar(2)
+l_motor = wpilib.Jaguar(l_motor_pwm)
+r_motor = wpilib.Jaguar(r_motor_pwm)
 
 #CAN Motors
 
 # shooter: -1 is full on
-shooter_motor = wpilib.CANJaguar(7)
+shooter_motor = wpilib.CANJaguar(shooter_wheel_can)
 shooter_motor.SetSpeedReference(wpilib.CANJaguar.kSpeedRef_QuadEncoder)
 shooter_motor.ConfigEncoderCodesPerRev(360)
 shooter_motor.ConfigNeutralMode(wpilib.CANJaguar.kNeutralMode_Brake)
 
-angle_motor = wpilib.CANJaguar(4)
+angle_motor = wpilib.CANJaguar(angle_motor_can)
 angle_motor.SetPositionReference(wpilib.CANJaguar.kPosRef_Potentiometer)
 angle_motor.ConfigPotentiometerTurns(1)
 angle_motor.ConfigNeutralMode(wpilib.CANJaguar.kNeutralMode_Brake)
 
-loader_cam_motor = wpilib.CANJaguar(3)
+loader_cam_motor = wpilib.CANJaguar(loader_cam_can)
 
 
 # optical sensors
-loader_sensor = wpilib.AnalogChannel(3)
-loader_cam_sensor = wpilib.AnalogChannel(4)
-shooter_detect_sensor = wpilib.AnalogChannel(5)
+loader_sensor = wpilib.AnalogChannel(loader_sensor_analog)
+loader_cam_sensor = wpilib.AnalogChannel(loader_cam_sensor_analog)
+shooter_detect_sensor = wpilib.AnalogChannel(shooter_detect_sensor)
+
+# compressor
+compressor = wpilib.Compressor(compressor_switch, compressor_relay)
+compressor.Start()
+
+# solenoids
+valve1 = wpilib.Solenoid(valve1_channel)
+valve2 = wpilib.Solenoid(valve2_channel)
 
 
 # drive object
@@ -41,6 +85,8 @@ drive = wpilib.RobotDrive(l_motor, r_motor)
 
 # TODO: Fix this. 
 drive.SetSafetyEnabled(False)
+
+
 
 class MyRobot(wpilib.SimpleRobot):
 
