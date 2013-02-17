d330ba0d9a02c4fd561b2043f8552f07fcea6fc8
Ethan Cox <ethanecx@gmail.com>
2013-02-17 14:55:46 -0500

    Added a is_sensor_working function to feeder

--- robot/source/components/feeder.py
+++ robot/source/components/feeder.py
@@ -37,12 +37,13 @@ class Feeder():
     
     '''Contains all the functions that control the cam motor'''
 
-    def __init__(self, feed_motor, frisbee_sensor, feed_sensor):
+    def __init__(self, feed_motor, frisbee_sensor, feed_sensor, frisbee_sensor_switch):
         
         '''saves all the variables, sets the state, and sets the motor speed to zero'''
         self.feed_motor = feed_motor
         self.frisbee_sensor = frisbee_sensor
         self.feed_sensor = feed_sensor
+        self.frisbee_sensor_switch = frisbee_sensor_switch
         self.feed_motor.Set(INITIAL_SPEED)
         self.updated = None
         self.state = STATE_READY
@@ -102,26 +103,36 @@ class Feeder():
             self.updated = IS_NOT_UPDATED
             self.state = STATE_FEED
         
+    def is_sensor_working(self):
+        if self.frisbee_sensor_switch == True:
+            return True
+        elif self.frisbee_sensor_switch == False:
+            return False
+        
     def get_frisbee_count(self):
         
-        '''Gets the distance away an object is from the sensor based on the voltage of the sensor'''
-        self.frisbee_count = None
-        self.distance = self.frisbee_sensor.GetDistance()
-        
-        if self.distance <= ONE_FRISBEE and self.distance >= TWO_FRISBEE:
+        if self.is_sensor_working(self) == False:
             self.frisbee_count = 1
+            
+        else:
+            '''Gets the distance away an object is from the sensor based on the voltage of the sensor'''
+            self.frisbee_count = None
+            self.distance = self.frisbee_sensor.GetDistance()
         
-        elif self.distance <= TWO_FRISBEE and self.distance >= THREE_FRISBEE:
-            self.frisbee_count = 2
+            if self.distance <= ONE_FRISBEE and self.distance >= TWO_FRISBEE:
+                self.frisbee_count = 1
         
-        elif self.distance <= THREE_FRISBEE and self.distance >= FOUR_FRISBEE:
-            self.frisbee_count = 3
+            elif self.distance <= TWO_FRISBEE and self.distance >= THREE_FRISBEE:
+                self.frisbee_count = 2
+        
+            elif self.distance <= THREE_FRISBEE and self.distance >= FOUR_FRISBEE:
+                self.frisbee_count = 3
 
-        elif self.distance <= FOUR_FRISBEE and self.distance > 1:
-            self.frisbee_count = 4
+            elif self.distance <= FOUR_FRISBEE and self.distance > 1:
+                self.frisbee_count = 4
        
-        elif self.distance > ONE_FRISBEE:
-            self.frisbee_count = 0
+            elif self.distance > ONE_FRISBEE:
+                self.frisbee_count = 0
             
         
         return self.frisbee_count
