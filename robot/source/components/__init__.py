d8cb0835bce998986285e7c0f99a76bc7f0e004c
srosenblum <rosenblum.sam@gmail.com>
2013-02-17 02:51:27 -0500

    Change components __init__.py

--- robot/source/components/__init__.py
+++ robot/source/components/__init__.py
@@ -1,6 +1,1 @@
 
-from .feeder import Feeder
-from .shooter_wheel import ShooterWheel
-from .driving import Driving
-from .frisbee_speed import FrisbeeSpeed
-from .target_detector import TargetDetector
