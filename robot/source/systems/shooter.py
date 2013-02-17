beb07cb270c0a3af459e6a02eba59117bcbbbd82
srosenblum <rosenblum.sam@gmail.com>
2013-02-17 17:52:58 -0500

    modified shooter.py

--- robot/source/systems/shooter.py
+++ robot/source/systems/shooter.py
@@ -2,72 +2,30 @@ from components import shooter_platform
 from components import feeder
 
 class Shooter():
-
-    def __init__(self, d_angle, d_speed):
-        
-        self.Updated = None
-        self.d_angle = d_angle
-        self.d_speed = d_speed
-    def shoot_when_ready(self):
-            
-            if shooter_wheel.ShooterWheel.is_ready_angle(self, self.d_angle) == True and shooter_wheel.ShooterWheel.is_ready_speed() == True:
-            
-                self.Updated = True
-                
-            elif shooter_wheel.ShooterWheel.is_ready_angle(self, self.d_angle) == False and shooter_wheel.ShooterWheel.is_ready_speed() == True:
-                
-                self.updated = 3
-                
-            elif shooter_wheel.ShooterWheel.is_ready_angle(self, self.d_angle) == True and shooter_wheel.ShooterWheel.is_ready_speed() == False:
-                
-                self.updated = 4
-    def continous_shoot(self):
+    '''
+        shooter class which controls the interactions of the feeder and the 
+        shooter_platform 
+    '''
     
-        while feeder.Feeder.get_frisbee_count() > 0:
-        
-            if shooter_wheel.ShooterWheel.is_ready_angle() == True and shooter_wheel.ShooterWheel.is_ready_speed() == True:
+    def __init__(self, shooter_platfrom, feeder, driver = None):
+        '''
+            Initializes shooter class
             
-                self.Updated = True
-                
-            elif shooter_wheel.ShooterWheel.is_ready_angle(self, self.d_angle) == False and shooter_wheel.ShooterWheel.is_ready_speed() == True:
-                
-                self.updated = 3
-                
-            elif shooter_wheel.ShooterWheel.is_ready_angle(self, self.d_angle) == True and shooter_wheel.ShooterWheel.is_ready_speed() == False:
-                
-                self.updated = 4
-            
-    def shoot_now(self):
+            param:shooter_platform - a shooter_platform object, all setup
+            param:feeder - a feeder object all setup
+        '''
     
-        self.updated = True
-        
-    def set_angle(self):
-        
-        self.updated = 1 
-        
-    def set_speed(self):
-        
-        self.updated = 2
+        self.shooter_platform = shooter_platform
+        self.feeder = feeder
+        self.driver = driver
+        self.state = None
+    
+    def shoot_if_ready(self):
+        ''' if motor is ready and there are frisbees set feeder to kick frisbee '''
+        if  self.shooter_platform.is_ready()  and \
+            self.feeder.has_frisbee():
+            feeder.feed_auto()
         
     def update(self):
-        
-        if self.updated == True:
-        
-            feeder.Feeder.feed()
-            self.Updated = None
-            
-        if self.updated == 1:
-            
-            shooter_wheel.ShooterWheel.set_angle(self, self.d_angle)
-            
-        if self.updated == 2:
-            
-            shooter_wheel.ShooterWheel.set_angle(self, self.d_speed)
-            
-        if self.updated == 3:
-            
-            self.set_angle()
-        
-        if self.updated == 4:
-            
-            self.set_speed()
+        ''' nothing to update so we should just pass '''
+        pass
