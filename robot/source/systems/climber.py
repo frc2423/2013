c7eb91565e8b1ffbc6989208da90ce72872f04af
Dustin Spicuzza <dustin@virtualroadside.com>
2013-02-18 23:50:31 -0500

    Added climber system

---
+++ robot/source/systems/climber.py
@@ -0,0 +1,26 @@
+
+class ClimberSystem(object):
+    
+    def __init__(self, climber, shooter_platform):
+        self.climber = climber
+        self.shooter_platform = shooter_platform
+        
+    def climb(self):
+        '''Call this to cause the robot to climb'''
+        if self.shooter_platform.at_zero():
+            self.climber.climb()
+        else:
+            self.shooter_platform.set_angle_manual(self.shooter_platform.LOWER_ANGLE_SPEED)
+    
+    def lower(self):
+        '''Call this to lower the robot down to the ground'''
+        self.climber.lower()
+    
+    def position(self):
+        '''This returns the current climber position'''
+        return self.climber.position()
+    
+    def update(self):
+        pass
+
+        
