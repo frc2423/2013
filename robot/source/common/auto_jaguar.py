aefa6eb73c04269874ddb5785f55a76887b7ee55
Dustin Spicuzza <dustin@virtualroadside.com>
2013-02-19 13:36:21 -0500

    Add position scaling stuff to auto jaguar
    - Still needs to be tested on the robot

--- robot/source/common/auto_jaguar.py
+++ robot/source/common/auto_jaguar.py
@@ -97,6 +97,51 @@ class PositionJaguar(_AutoJaguar):
         '''Tell the motor to go to a specific position'''
         self.mode = self.AUTO
         self.value = position
+        
+class AnglePositionJaguar(_AutoJaguar):
+    '''
+        Allows setting a position based on an angle
+        
+        TODO: Does the scaling belong in EzCANJaguar instead?
+    '''
+        
+    AUTO = CANJaguar.kPosition
+    
+    def __init__(self, motor, threshold, pmin, pmax, min_angle, max_angle):
+        '''See constructor for AutoJaguar
+        
+            :param pmin:        Min value of the jaguar
+            :param pmax:        Max value of the jaguar
+            :param min_angle:   Angle the min position represents
+            :param max_angle:   Angle the max position represents
+        '''
+        self.get_value = motor.GetPosition
+        _AutoJaguar.__init__(self, motor, threshold)
+        
+        self.pmin = pmin
+        self.pmax = pmax
+        self.amin = min_angle
+        self.amax = max_angle
+        
+    def get_position(self):
+        '''Returns current position as calculated by the position reference'''
+        p = self.motor.GetPosition()
+        
+        # x is angle
+        # y is position
+        
+        # Xmax - (Ymax - Y)( (Xmax - Xmin) / (Ymax - Ymin) )
+        return self.amax - (self.pmax - p)*((self.amax - self.amin) / float(self.pmax - self.pmin))
+    
+    def set_angle(self, angle):
+        '''Tell the motor to go to a specific angle'''
+        self.mode = self.AUTO
+        
+        # x is position
+        # y is angle
+        
+        # Xmax - (Ymax - Y)( (Xmax - Xmin) / (Ymax - Ymin) )
+        self.value = self.pmax - (self.amax - angle)*((self.pmax - self.pmin) / float(self.amax - self.amin))
     
     
 class SpeedJaguar(_AutoJaguar):
